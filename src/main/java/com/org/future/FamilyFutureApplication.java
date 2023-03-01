fdb0aad99ebc49739fc008f6d96fb2772d59510a
srinugithub9676 <githubsolution@gmail.com>
2023-03-01 16:26:11 +0530

    main changes

--- src/main/java/com/org/future/FamilyFutureApplication.java
+++ src/main/java/com/org/future/FamilyFutureApplication.java
@@ -2,9 +2,16 @@ package com.org.future;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class FamilyFutureApplication {
+public class FamilyFutureApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) 
+	{
+	return application.sources(FamilyFutureApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(FamilyFutureApplication.class, args);
