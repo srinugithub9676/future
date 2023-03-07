c167fcb3a9d55aa759d764bb996c63bcff36fbc3
srinugithub9676 <githubsolution@gmail.com>
2023-03-07 11:53:09 +0530

    test

--- src/main/java/com/org/future/DesignPatteran/SmsNotification.java
+++ src/main/java/com/org/future/DesignPatteran/SmsNotification.java
@@ -4,7 +4,7 @@ public class SmsNotification extends Notification{
 
 	@Override
 	public void notification() {
-		// TODO Auto-generated method stub
+	
 		System.out.println("this is sms notification ");
 	}
 
